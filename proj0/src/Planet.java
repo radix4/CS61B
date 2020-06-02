81f5c192202b8b9750516999ea0ded7ee3faca09
Thang Cao <shiyoline@gmail.com>
2020-06-01 21:01:26 -0700

    implemented calcDistance method

---
+++ proj0/src/Planet.java
@@ -0,0 +1,40 @@
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    public Planet(double xxPos, double yyPos, double xxVel,
+                  double yyVel, double mass, String imgFileName) {
+        this.xxPos = xxPos;
+        this.yyPos = yyPos;
+        this.xxVel = xxVel;
+        this.yyVel = yyVel;
+        this.mass = mass;
+        this.imgFileName = imgFileName;
+    }
+
+    public Planet(Planet p){
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+    }
+
+
+    /** takes in a planet object and calculate the distance
+     * between this planet and planet p */
+    public double calcDistance(Planet p){
+        double r = 0;
+        double dx = p.xxPos - this.xxPos;
+        double dy = p.yyPos - this.yyPos;
+
+        r = Math.sqrt(dx*dx + dy*dy);
+
+        return r;
+    }
+}
