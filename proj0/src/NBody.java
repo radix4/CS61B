6a47e705b16bdacf65bb48ce1260c67b0eb01393
Thang Cao <shiyoline@gmail.com>
2020-06-03 13:17:02 -0700

    NBody class completed

--- proj0/src/NBody.java
+++ proj0/src/NBody.java
@@ -21,38 +21,30 @@ public class NBody {
         double radius = readRadius(fileName);
         Planet[] planets = readPlanets(fileName);
 
-        List<Double> xForces  = new ArrayList<Double>();
-        List<Double> yForces  = new ArrayList<Double>();
-
         StdDraw.enableDoubleBuffering();
+        StdDraw.setScale(-radius,radius);
 
+        for (time = 0; time <= T; time += dt) {
+            Double[] xForces = new Double[planets.length];
+            Double[] yForces = new Double[planets.length];
 
-        for (time = 0; time < T; time += dt){
-            StdDraw.setScale(-radius,radius);
-            StdDraw.picture (0,0,"images/starfield.jpg");
-
-            for (Planet p : planets){
-                //xForces.add(p.calcNetForceExertedByX(planets));
-                //yForces.add(p.calcNetForceExertedByY(planets));
-
-                p.update(dt,p.calcNetForceExertedByX(planets), p.calcNetForceExertedByY(planets));
-
-                p.draw();
-                StdDraw.show();
+            for (int i = 0; i < planets.length; i++){
+                xForces[i] = planets[i].calcNetForceExertedByX(planets);
+                yForces[i] = planets[i].calcNetForceExertedByY(planets);
             }
 
-            //StdDraw.pause(10);
-            StdDraw.clear();
-        }
+            for (int i = 0; i < planets.length; i++) {
+                planets[i].update(dt,xForces[i],yForces[i]);
+            }
 
-        StdOut.printf("%d\n", planets.length);
-        StdOut.printf("%.2e\n", radius);
-        for (int i = 0; i < planets.length; i++) {
-            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
-                    planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
-                    planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
-        }
+            StdDraw.picture(0, 0, "images/starfield.jpg");
+            for(Planet p : planets){
+                p.draw();
+            }
 
+            StdDraw.show();
+            StdDraw.pause(5);
+        }
     }
 
 
