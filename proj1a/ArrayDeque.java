173073f646cd0e4a654b566387d74c088ffc549b
Thang Cao <shiyoline@gmail.com>
2020-06-05 18:44:04 -0700

    added ArrayDeque and LinkedListDeque classes

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,29 @@
+public class ArrayDeque<T> {
+    public void addFirst(T item){
+
+    }
+
+    public void addLast(T item){
+
+    }
+
+    public boolean isEmpty(){
+        return true;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public T removeFirst(){
+        return null;
+    }
+
+    public T removeLast(){
+        return null;
+    }
+
+    public T get(int index){
+        return null;
+    }
+}
