6a850e593e13331d832eaf00172309829ed8ada5
Thang Cao <shiyoline@gmail.com>
2020-06-08 22:27:31 -0700

    Setting up proj1a 2nd part (ArrayDeque).

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,10 +1,31 @@
 public class ArrayDeque<T> {
+    private T[] array;
+    private int size;
+
+    public ArrayDeque() {
+        array = (T[]) new Object[8];
+        size = 0;
+    }
+
     public void addFirst(T item){
 
     }
 
     public void addLast(T item){
 
+    }
+
+    public T getFirst(){
+
+        return null;
+    }
+
+    public T getLast(){
+
+        return null;
+    }
+
+    public void printDeque() {
     }
 
     public boolean isEmpty(){
@@ -24,6 +45,10 @@ public class ArrayDeque<T> {
     }
 
     public T get(int index){
+        return null;
+    }
+
+    public T getRecursive(int index){
         return null;
     }
 }
