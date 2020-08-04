e9bd7dd9fa14b505d9b0926e571d1b2a57bb216c
Thang Cao <shiyoline@gmail.com>
2020-08-03 22:23:04 -0700

    Implemented QuickFindDS class.

---
+++ Sandbox/src/DisjointSets/QuickFindDS.java
@@ -0,0 +1,30 @@
+package DisjointSets;
+
+public class QuickFindDS implements DisjointSets{
+    private int[] id;
+
+    /* Θ(N) */
+    public QuickFindDS(int N){
+        id = new int[N];
+        for (int i = 0; i < N; i++){
+            id[i] = i;
+        }
+    }
+
+    /* need to iterate through the array => Θ(N) */
+    @Override
+    public void connect(int p, int q) {
+        int pid = id[p];
+        int qid = id[q];
+        for (int i = 0; i < id.length; i++){
+            if (id[i] == pid){
+                id[i] = qid;
+            }
+        }
+    }
+
+    @Override
+    public boolean isConnected(int p, int q) {
+        return (id[p] == id[q]);
+    }
+}
