899caf34f60e98a08dcebada2017024cff28f517
Thang Cao <shiyoline@gmail.com>
2020-08-07 10:03:56 -0700

    Added PriorityQueue Interface class.

---
+++ Sandbox/src/HeapsAndPriorityQueues/PriorityQueueInterface.java
@@ -0,0 +1,14 @@
+package HeapsAndPriorityQueues;
+
+/** (Min) Priority Queue: Allowing tracking and removal of
+ * the smallest item in a priority queue. */
+public interface PriorityQueueInterface<Item> {
+    /** Adds the item to the priority queue. */
+    public void add(Item x);
+    /** Returns the smallest item in the priority queue. */
+    public Item getSmallest();
+    /** Removes the smallest item from the priority queue. */
+    public Item removeSmallest();
+    /** Returns the size of the priority queue. */
+    public int size();
+}
